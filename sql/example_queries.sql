73e7432ce10a3d3770cffc0ad5b8d3290e548154
Evan Wieland <hello@bitsmithy.io>
2021-04-30 15:52:22 -0500

    did some things

---
+++ sql/example_queries.sql
@@ -0,0 +1,24 @@
+-- Get the hashtag text on every one of a particular user's posts with a hashtag.
+SELECT POST.UserID, HASHTAG.Hashtag
+FROM HASHTAG
+INNER JOIN POST ON HASHTAG.HashtagID = POST.HashtagID AND UserId = 1;
+
+
+-- Get the first user to join the platform.
+SELECT UserID, Username, DateJoined
+FROM USER
+ORDER BY DateJoined ASC LIMIT 1;
+
+
+-- Get the number of likes each user has given.
+SELECT UserID, COUNT(LikeID) AS LikesGiven
+FROM LIKES
+GROUP BY UserID;
+
+
+-- Get all posts a certain user liked on a certain day.
+SELECT *
+FROM POST
+WHERE PostID IN (SELECT DISTINCT PostID
+FROM LIKES
+WHERE UserID = 1 AND DateLiked LIKE '2019-09-14%')
